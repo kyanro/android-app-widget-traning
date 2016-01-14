e0382a76bc17b40d83e92fe96b5e57d1fa1b1fec
kyanro <gw.fnct+github@gmail.com>
2016-01-14 16:19:09 +0900

    ボタンのクリックイベントを取れるようになったよ！

--- app/src/main/java/com/kyanro/widgettraining/NewAppWidget.java
+++ app/src/main/java/com/kyanro/widgettraining/NewAppWidget.java
@@ -1,9 +1,13 @@
 package com.kyanro.widgettraining;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
@@ -11,13 +15,30 @@ import android.widget.RemoteViews;
  */
 public class NewAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    public static final String ACTION_UP_CLICK = "com.kyanro.widgettraining.ACTION_UP_CLICK";
+    public static final String ACTION_DOWN_CLICK = "com.kyanro.widgettraining.ACTION_DOWN_CLICK";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
         CharSequence widgetText = NewAppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // click event 送信
+        Log.d("mydevlog", "update log");
+
+        Toast.makeText(context, "test", Toast.LENGTH_LONG).show();
+
+        Intent upIntent = new Intent();
+        upIntent.setAction(ACTION_UP_CLICK);
+        PendingIntent upPendingIntent = PendingIntent.getBroadcast(context, 0, upIntent, 0);
+        views.setOnClickPendingIntent(R.id.up_text, upPendingIntent);
+
+        Intent downIntent = new Intent();
+        downIntent.setAction(ACTION_DOWN_CLICK);
+        PendingIntent downPendingIntent = PendingIntent.getBroadcast(context, 0, downIntent, 0);
+        views.setOnClickPendingIntent(R.id.down_text, downPendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -25,6 +46,8 @@ public class NewAppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
@@ -47,6 +70,20 @@ public class NewAppWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        Log.d("mydevlog", "intent comming");
+
+        String action = intent.getAction();
+        if (ACTION_UP_CLICK.equals(action)) {
+            Log.d("mydevlog", "button up clicked");
+        }else if (ACTION_DOWN_CLICK.equals(action)) {
+            Log.d("mydevlog", "button down clicked");
+        }
     }
 }
 
